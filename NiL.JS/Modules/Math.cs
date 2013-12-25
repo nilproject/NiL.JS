064b3042b1ec6c8ef45cff5afa18cb604e0e366c
Дмитрий <polhovich.dm@mail.ru>
2013-12-25 15:50:35 +0700

    Update Math.cs

--- NiL.JS/Modules/Math.cs
+++ NiL.JS/Modules/Math.cs
@@ -1,15 +1,16 @@
-﻿using NiL.JS.Core;
+using NiL.JS.Core;
 using NiL.JS.Core.BaseTypes;
 using System;
 
 namespace NiL.JS.Modules
 {
+    [Singleton]
     internal sealed class Math
     {
         [Protected]
         public const double E = System.Math.E;
 
-        public JSObject pow(IContextStatement[] args)
+        public static JSObject pow(IContextStatement[] args)
         {
             if (args.Length < 2)
                 return double.NaN;
@@ -27,9 +28,7 @@ namespace NiL.JS.Modules
             {
                 int ix = 0;
                 string s = r.oValue as string;
-                if (!double.TryParse(s, out x) && (s.Length > 2) && (s[0] == '0') && (s[1] == 'x'))
-                    if (int.TryParse(s.Substring(2), System.Globalization.NumberStyles.HexNumber, null, out ix))
-                        x = ix;
+                Parser.ParseNumber(s, ref ix, false, out x);
             }
 
             r = args[1].Invoke();
@@ -44,15 +43,13 @@ namespace NiL.JS.Modules
             {
                 int ix = 0;
                 string s = r.oValue as string;
-                if (!double.TryParse(s, out x) && (s.Length > 2) && (s[0] == '0') && (s[1] == 'x'))
-                    if (int.TryParse(s.Substring(2), System.Globalization.NumberStyles.HexNumber, null, out ix))
-                        y = ix;
+                Parser.ParseNumber(s, ref ix, false, out y);
             }
 
             return System.Math.Pow(x, y);
         }
 
-        public JSObject floor(IContextStatement[] args)
+        public static JSObject floor(IContextStatement[] args)
         {
             if (args.Length < 1)
                 return double.NaN;
@@ -64,16 +61,36 @@ namespace NiL.JS.Modules
                 x = r.dValue;
             else if ((r.ValueType == ObjectValueType.Statement) || (r.ValueType == ObjectValueType.Undefined))
                 return double.NaN;
-            else if ((r.ValueType == ObjectValueType.Object) && (r.oValue is string))
+            else if ((r.ValueType == ObjectValueType.String))
             {
                 int ix = 0;
                 string s = r.oValue as string;
-                if (!double.TryParse(s, out x) && (s.Length > 2) && (s[0] == '0') && (s[1] == 'x'))
-                    if (int.TryParse(s.Substring(2), System.Globalization.NumberStyles.HexNumber, null, out ix))
-                        x = ix;
+                Parser.ParseNumber(s, ref ix, false, out x);
             }
 
             return System.Math.Floor(x);
+        }
+
+        public static JSObject round(IContextStatement[] args)
+        {
+            if (args.Length < 1)
+                return double.NaN;
+            var r = args[0].Invoke();
+            double x = double.NaN;
+            if (r.ValueType == ObjectValueType.Int || r.ValueType == ObjectValueType.Bool)
+                x = r.iValue;
+            else if (r.ValueType == ObjectValueType.Double)
+                x = r.dValue;
+            else if ((r.ValueType == ObjectValueType.Statement) || (r.ValueType == ObjectValueType.Undefined))
+                return double.NaN;
+            else if ((r.ValueType == ObjectValueType.String))
+            {
+                int ix = 0;
+                string s = r.oValue as string;
+                Parser.ParseNumber(s, ref ix, false, out x);
+            }
+
+            return (int)System.Math.Round(x);
         }
     }
 }
