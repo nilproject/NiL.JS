73454a54f758f1be9dd6367013b5ebfa8a80cf31
iaiojeg <polhovich.dm@mail.ru>
2013-12-30 03:11:16 +0700

    Add TypeOf.cs

---
+++ NiL.JS/Statements/Operators/TypeOf.cs
@@ -0,0 +1,55 @@
+﻿using NiL.JS.Core;
+using System;
+
+namespace NiL.JS.Statements.Operators
+{
+    internal class TypeOf : Operator
+    {
+        public TypeOf(Statement first, Statement second)
+            : base(first, second)
+        {
+
+        }
+
+        public override JSObject Invoke(Context context)
+        {
+            var val = first.Invoke(context);
+            var vt = val.ValueType;
+            switch (vt)
+            {
+                case ObjectValueType.Int:
+                case ObjectValueType.Double:
+                    {
+                        return "number";
+                    }
+                case ObjectValueType.NotExist:
+                case ObjectValueType.NotExistInObject:
+                case ObjectValueType.Undefined:
+                    {
+                        return "undefined";
+                    }
+                case ObjectValueType.String:
+                    {
+                        return "string";
+                    }
+                case ObjectValueType.Bool:
+                    {
+                        return "boolean";
+                    }
+                case ObjectValueType.Statement:
+                    {
+                        return "function";
+                    }
+                case ObjectValueType.Date:
+                case ObjectValueType.Object:
+                    {
+                        if (val.oValue is NiL.JS.Core.BaseTypes.String)
+                            return "string";
+                        else
+                            return "object";
+                    }
+                default: throw new NotImplementedException();
+            }
+        }
+    }
+}
