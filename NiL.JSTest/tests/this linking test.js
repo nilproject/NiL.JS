8d5a55395ccdda326c950241453d62fef90dc107
Дмитрий <polhovich.dm@mail.ru>
2014-01-31 01:52:13 -0800

    Update test

--- NiL.JSTest/tests/this linking test.js
+++ NiL.JSTest/tests/this linking test.js
@@ -20,3 +20,15 @@ function (a) {
 
 if (function () { return this } () != this)
     console.log("#3.");
+
+var o1 = { x: 4, set p(v){this.x = v;} };
+var o2 = { y: 3, set p(v){this.y = v;} };
+o2.p = (o1.x / 2);
+if (o1.y)
+    $ERROR("#4");
+if (o1.x != 4)
+    $ERROR("#5");
+if (o2.y != 2)
+    $ERROR("#6");
+if (o2.x)
+    $ERROR("#7");
