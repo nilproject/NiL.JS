fd1408a433efaa5e6266190a736f0271aa3845cb
Дмитрий <polhovich.dm@mail.ru>
2014-04-18 14:59:27 +0700

    Сборка под NET40

--- NiL.JS/Core/Backward.cs
+++ NiL.JS/Core/Backward.cs
@@ -15,7 +15,8 @@ namespace System
     internal delegate TResult Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, TResult>(T1 prm1, T2 prm2, T3 prm3, T4 prm4, T5 prm5, T6 prm6, T7 prm7, T8 prm8, T9 prm9, T10 prm10, T11 prm11, T12 prm12, T13 prm13, T14 prm14);
     internal delegate TResult Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, TResult>(T1 prm1, T2 prm2, T3 prm3, T4 prm4, T5 prm5, T6 prm6, T7 prm7, T8 prm8, T9 prm9, T10 prm10, T11 prm11, T12 prm12, T13 prm13, T14 prm14, T15 prm15);
     internal delegate TResult Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, TResult>(T1 prm1, T2 prm2, T3 prm3, T4 prm4, T5 prm5, T6 prm6, T7 prm7, T8 prm8, T9 prm9, T10 prm10, T11 prm11, T12 prm12, T13 prm13, T14 prm14, T15 prm15, T16 prm16);
-
+#endif
+#if NET35 || NET40
     public static class ParameterInfoExtension
     {
         public static Object GetCustomAttribute(this ParameterInfo _this, Type attributeType)
