945b7c945ac410d42c2a4eba6315bad1522d3b5d
NiLProject <polhovich.dm@mail.ru>
2014-11-02 12:21:32 +0700

    Version Update

---
+++ NiL.JS/Core/PropertyPair.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NiL.JS.Core.BaseTypes;
+
+namespace NiL.JS.Core
+{
+    public sealed class PropertyPair
+    {
+        internal Function get;
+        internal Function set;
+
+        public Function Getter { get { return get; } }
+        public Function Setter { get { return set; } }
+    }
+}
