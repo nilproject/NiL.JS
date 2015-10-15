815c2936b70d6389d4e8b7398dc16d3d7b5d7f46
nilproject <polhovich.dm@mail.ru>
2015-10-15 22:40:53 +0600

    Optimization

--- NiL.JS/Core/StringMap2.cs
+++ NiL.JS/Core/StringMap2.cs
@@ -26,14 +26,20 @@ namespace NiL.JS.Core
 
         private static readonly Record[] emptyRecords = new Record[0];
 
-        private bool emptyKeyValueExists = false;
-        private TValue emptyKeyValue;
+        private bool _emptyKeyValueExists = false;
+        private TValue _emptyKeyValue;
 
-        private Record[] records = emptyRecords;
-        private int[] ExistsedIndexes;
+        private Record[] _records = emptyRecords;
+        private int[] _existsedIndexes;
 
-        private int count;
-        private int eicount;
+        private int _count;
+        private int _eicount;
+        private int _previousIndex;
+
+        public StringMap2()
+        {
+            Clear();
+        }
 
         private void insert(string key, TValue value, int hash, bool @throw)
         {
@@ -41,63 +47,63 @@ namespace NiL.JS.Core
                 throw new ArgumentNullException();
             if (key.Length == 0)
             {
-                if (@throw && emptyKeyValueExists)
+                if (@throw && _emptyKeyValueExists)
                     throw new InvalidOperationException("Item already Exists");
-                emptyKeyValueExists = true;
-                emptyKeyValue = value;
+                _emptyKeyValueExists = true;
+                _emptyKeyValue = value;
                 //count++;
                 return;
             }
-            var elen = records.Length - 1;
-            if (records.Length == 0)
+            var elen = _records.Length - 1;
+            if (_records.Length == 0)
                 elen = increaseSize() - 1;
             int index;
             int colisionCount = 0;
             index = hash & elen;
             do
             {
-                if (records[index].hash == hash && string.CompareOrdinal(records[index].key, key) == 0)
+                if (_records[index].hash == hash && string.CompareOrdinal(_records[index].key, key) == 0)
                 {
                     if (@throw)
                         throw new InvalidOperationException("Item already Exists");
-                    records[index].value = value;
+                    _records[index].value = value;
                     return;
                 }
-                index = records[index].next - 1;
+                index = _records[index].next - 1;
             }
             while (index >= 0);
             // не нашли
 
-            if ((count > 50 && count * 9 / 5 >= elen) || count == elen + 1)
+            if ((_count > 50 && _count * 9 / 5 >= elen) || _count == elen + 1)
                 elen = increaseSize() - 1;
             int prewIndex = -1;
             index = hash & elen;
 
-            if (records[index].key != null)
+            if (_records[index].key != null)
             {
-                while (records[index].next > 0)
+                while (_records[index].next > 0)
                 {
-                    index = records[index].next - 1;
+                    index = _records[index].next - 1;
                     colisionCount++;
                 }
                 prewIndex = index;
-                while (records[index].key != null)
+                while (_records[index].key != null)
                     index = (index + 61) & elen;
             }
-            records[index].hash = hash;
-            records[index].key = key;
-            records[index].value = value;
+            _records[index].hash = hash;
+            _records[index].key = key;
+            _records[index].value = value;
             if (prewIndex >= 0)
-                records[prewIndex].next = index + 1;
-            if (eicount == ExistsedIndexes.Length)
+                _records[prewIndex].next = index + 1;
+            if (_eicount == _existsedIndexes.Length)
             {
                 // Увеличиваем размер массива с занятыми номерами
-                var newEI = new int[ExistsedIndexes.Length << 1];
-                Array.Copy(ExistsedIndexes, newEI, ExistsedIndexes.Length);
-                ExistsedIndexes = newEI;
+                var newEI = new int[_existsedIndexes.Length << 1];
+                Array.Copy(_existsedIndexes, newEI, _existsedIndexes.Length);
+                _existsedIndexes = newEI;
             }
-            ExistsedIndexes[eicount++] = index;
-            count++;
+            _existsedIndexes[_eicount++] = index;
+            _count++;
 
             if (colisionCount > 17)
                 increaseSize();
@@ -126,27 +132,33 @@ namespace NiL.JS.Core
             value = default(TValue);
             if (key.Length == 0)
             {
-                if (!emptyKeyValueExists)
+                if (!_emptyKeyValueExists)
                 {
                     return false;
                 }
-                value = emptyKeyValue;
+                value = _emptyKeyValue;
                 return true;
             }
-            if (records.Length == 0)
+            if (_records.Length == 0)
             {
                 return false;
             }
+            if (_previousIndex != -1 && string.CompareOrdinal(_records[_previousIndex].key, key) == 0)
+            {
+                value = _records[_previousIndex].value;
+                return true;
+            }
             int hash = computeHash(key);
-            int index = hash & (records.Length - 1);
+            int index = hash & (_records.Length - 1);
             do
             {
-                if (records[index].hash == hash && string.CompareOrdinal(records[index].key, key) == 0)
+                if (_records[index].hash == hash && string.CompareOrdinal(_records[index].key, key) == 0)
                 {
-                    value = records[index].value;
+                    _previousIndex = index;
+                    value = _records[index].value;
                     return true;
                 }
-                index = records[index].next - 1;
+                index = _records[index].next - 1;
             }
             while (index >= 0);
             value = default(TValue);
@@ -164,58 +176,63 @@ namespace NiL.JS.Core
                 throw new ArgumentNullException();
             if (key.Length == 0)
             {
-                if (!emptyKeyValueExists)
+                if (!_emptyKeyValueExists)
                 {
                     return false;
                 }
-                emptyKeyValue = default(TValue);
-                emptyKeyValueExists = false;
+                _emptyKeyValue = default(TValue);
+                _emptyKeyValueExists = false;
                 return true;
             }
-            if (records.Length == 0)
+            if (_records.Length == 0)
                 return false;
-            var elen = records.Length - 1;
+            var elen = _records.Length - 1;
             int hash;
             int index;
-            int targetIndex = -1, prewIndex;
+            int targetIndex = -1;
+            int prewIndex;
             hash = key[0];
             index = hash & elen;
             hash = computeHash(key);
             //hash = key.GetHashCode();
-            for (index = hash & elen; index >= 0; index = records[index].next - 1)
+            for (index = hash & elen; index >= 0; index = _records[index].next - 1)
             {
-                if (records[index].hash == hash && string.CompareOrdinal(records[index].key, key) == 0)
+                if (_records[index].hash == hash && string.CompareOrdinal(_records[index].key, key) == 0)
                 {
-                    if (records[index].next > 0)
+                    if (_records[index].next > 0)
                     {
                         prewIndex = targetIndex;
                         targetIndex = index;
-                        index = records[index].next - 1;
+                        index = _records[index].next - 1;
                         do
                         {
-                            if ((records[index].hash & elen) >= targetIndex)
+                            if ((_records[index].hash & elen) >= targetIndex)
                             {
-                                records[targetIndex] = records[index];
-                                records[targetIndex].next = index + 1;
+                                _records[targetIndex] = _records[index];
+                                _records[targetIndex].next = index + 1;
                                 prewIndex = targetIndex;
                                 targetIndex = index;
                             }
-                            index = records[index].next - 1;
+                            index = _records[index].next - 1;
                         }
                         while (index >= 0);
-                        records[targetIndex].key = null;
-                        records[targetIndex].value = default(TValue);
-                        records[targetIndex].hash = 0;
+                        _records[targetIndex].key = null;
+                        _records[targetIndex].value = default(TValue);
+                        _records[targetIndex].hash = 0;
+                        if (targetIndex == _previousIndex)
+                            _previousIndex = -1;
                         if (prewIndex >= 0)
-                            records[prewIndex].next = 0;
+                            _records[prewIndex].next = 0;
                     }
                     else
                     {
-                        records[index].key = null;
-                        records[index].value = default(TValue);
-                        records[index].hash = 0;
+                        if (index == _previousIndex)
+                            _previousIndex = -1;
+                        _records[index].key = null;
+                        _records[index].value = default(TValue);
+                        _records[index].hash = 0;
                         if (targetIndex >= 0)
-                            records[targetIndex].next = 0;
+                            _records[targetIndex].next = 0;
                     }
                     return true;
                 }
@@ -227,28 +244,29 @@ namespace NiL.JS.Core
 
         private int increaseSize()
         {
-            if (records.Length == 0)
+            if (_records.Length == 0)
             {
-                records = new Record[4];
-                ExistsedIndexes = new int[4];
+                _records = new Record[4];
+                _existsedIndexes = new int[4];
             }
             else
             {
                 //if (count > 100 && records.Length / count > 50)
                 //    throw new InvalidOperationException();
-                var oldRecords = records;
-                records = new Record[records.Length << 1];
-                int i = 0, c = eicount;
-                count = 0;
-                eicount = 0;
+                var oldRecords = _records;
+                _records = new Record[_records.Length << 1];
+                int i = 0, c = _eicount;
+                _count = 0;
+                _eicount = 0;
                 for (; i < c; i++)
                 {
-                    var index = ExistsedIndexes[i];
+                    var index = _existsedIndexes[i];
                     if (oldRecords[index].key != null)
                         insert(oldRecords[index].key, oldRecords[index].value, oldRecords[index].hash, false);
                 }
             }
-            return records.Length;
+            _previousIndex = -1;
+            return _records.Length;
         }
 
         public void Add(string key, TValue value)
@@ -264,12 +282,12 @@ namespace NiL.JS.Core
 
         public ICollection<string> Keys
         {
-            get { return (from item in records where item.key != null select item.key).ToArray(); }
+            get { return (from item in _records where item.key != null select item.key).ToArray(); }
         }
 
         public ICollection<TValue> Values
         {
-            get { return (from item in records where item.key != null select item.value).ToArray(); }
+            get { return (from item in _records where item.key != null select item.value).ToArray(); }
         }
 
         public TValue this[string key]
@@ -294,13 +312,14 @@ namespace NiL.JS.Core
 
         public void Clear()
         {
-            if (ExistsedIndexes != null)
-                Array.Clear(ExistsedIndexes, 0, ExistsedIndexes.Length);
-            Array.Clear(records, 0, records.Length);
-            count = 0;
-            eicount = 0;
-            emptyKeyValue = default(TValue);
-            emptyKeyValueExists = false;
+            if (_existsedIndexes != null)
+                Array.Clear(_existsedIndexes, 0, _existsedIndexes.Length);
+            Array.Clear(_records, 0, _records.Length);
+            _count = 0;
+            _eicount = 0;
+            _emptyKeyValue = default(TValue);
+            _emptyKeyValueExists = false;
+            _previousIndex = -1;
         }
 
         public bool Contains(KeyValuePair<string, TValue> item)
@@ -315,7 +334,7 @@ namespace NiL.JS.Core
 
         public int Count
         {
-            get { return count; }
+            get { return _count; }
         }
 
         public bool IsReadOnly
@@ -330,12 +349,12 @@ namespace NiL.JS.Core
 
         public IEnumerator<KeyValuePair<string, TValue>> GetEnumerator()
         {
-            if (emptyKeyValueExists)
-                yield return new KeyValuePair<string, TValue>("", emptyKeyValue);
-            for (int i = 0; i < eicount; i++)
+            if (_emptyKeyValueExists)
+                yield return new KeyValuePair<string, TValue>("", _emptyKeyValue);
+            for (int i = 0; i < _eicount; i++)
             {
-                if (records[ExistsedIndexes[i]].key != null)
-                    yield return new KeyValuePair<string, TValue>(records[ExistsedIndexes[i]].key, records[ExistsedIndexes[i]].value);
+                if (_records[_existsedIndexes[i]].key != null)
+                    yield return new KeyValuePair<string, TValue>(_records[_existsedIndexes[i]].key, _records[_existsedIndexes[i]].value);
             }
         }
 
