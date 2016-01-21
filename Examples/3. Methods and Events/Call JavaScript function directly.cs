46361ec7c06c562cc2e68c5f16abc8931bf2ff8d
nilproject <polhovich.dm@mail.ru>
2016-01-21 20:22:43 +0600

    New example

---
+++ Examples/3. Methods and Events/Call JavaScript function directly.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NiL.JS.BaseLibrary;
+using NiL.JS.Core;
+using NiL.JS.Extensions;
+
+namespace Examples._3_Methods_and_Events
+{
+    public sealed class Call_JavaScript_function_directly : ExamplesFramework.Example
+    {
+        public override void Run()
+        {
+            var context = new Context();
+
+            context.Eval("var concat = (a, b) => a + ', ' + b");
+
+            var concatFunction = context.GetVariable("concat").As<Function>();
+
+            Console.WriteLine(concatFunction.Call(new Arguments { "Hello", "World!" })); // Console: 'Hello, World!'
+        }
+    }
+}
