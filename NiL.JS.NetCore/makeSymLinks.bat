fb30db968f72764dfb987609938721516f8a2e10
Полхович Дмитрий <polhovich.dm@mail.ru>
2016-07-18 00:39:30 +0600

    Symlinks generator for .Net Core project

--- NiL.JS.NetCore/makeSymLinks.bat
+++ NiL.JS.NetCore/makeSymLinks.bat
@@ -1,2 +1,2 @@
-for /f %%i in ('dir /a:d /b "../nil.js/"') do mklink /j %%i "../nil.js/%%i"
-for %%i in (../nil.js/*.cs) do mklink %%i "../nil.js/%%i"
+for /f %%i in ('dir /a:d /b "../nil.js/"') do rmdir "./%%i"
+for /f %%i in ('dir /a:d /b "../nil.js/"') do mklink /j %%i "./../nil.js/%%i"
