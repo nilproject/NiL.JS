770358a3a30f247b9fdc1fcb4b569afd8aaea3a9
Дмитрий <polhovich.dm@mail.ru>
2016-10-09 10:30:13 +0700

    Added functional tests for interop

---
+++ FunctionalTests/Interop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NiL.JS.BaseLibrary;
+using NiL.JS.Core;
+
+namespace FunctionalTests
+{
+    [TestClass]
+    public class Interop
+    {
+        [TestMethod]
+        public void CreateInstanceOfGenericType()
+        {
+            var context = new Context();
+            context.DefineConstructor(typeof(List<>));
+
+            var result = context.Eval("new (List(Number))()").Value;
+
+            Assert.AreSame(typeof(List<Number>), result.GetType());
+        }
+    }
+}
