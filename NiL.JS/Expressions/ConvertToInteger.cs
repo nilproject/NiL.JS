24dd60127b91b07d64920ab0e63cf7bd439ec0be
Дмитрий <polhovich.dm@mail.ru>
2017-03-22 01:28:55 +0700

    Fix build for Net 4.0

--- NiL.JS/Expressions/ConvertToInteger.cs
+++ NiL.JS/Expressions/ConvertToInteger.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using NiL.JS.Core;
 using System.Reflection;
+using NiL.JS.Core;
+
+#if NET40
+using NiL.JS.Backward;
+#endif
 
 namespace NiL.JS.Expressions
 {
