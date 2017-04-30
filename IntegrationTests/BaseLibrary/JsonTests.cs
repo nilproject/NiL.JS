c5c15c7897147772e3db15a4cfa4a856147020e1
Дмитрий <polhovich.dm@mail.ru>
2017-04-30 18:04:15 +0700

    New tests

---
+++ IntegrationTests/BaseLibrary/JsonTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NiL.JS.Core;
+
+namespace IntegrationTests.BaseLibrary
+{
+    [TestClass]
+    public sealed class JsonTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(JSException))]
+        [Timeout(1000)]
+        public void IncorrectJsonShouldBringToError()
+        {
+            string json = "\"a\":0";
+
+            NiL.JS.BaseLibrary.JSON.parse(json);
+
+            Assert.Fail();
+        }
+    }
+}
