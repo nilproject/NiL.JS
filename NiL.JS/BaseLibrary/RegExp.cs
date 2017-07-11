3bd1b407d255655f6a3854924be0cc0991dafbf7
Michael Schmidt <mitchi5000.ms@googlemail.com>
2017-07-11 21:33:26 +0200

    RegExp Update
    The 'u' and 'y' flag are now supported.
    Created Regexes are now cached for increased performance.
    'u' doesn't behave quite right because there is no support in the underlying Regex class.

--- NiL.JS/BaseLibrary/RegExp.cs
+++ NiL.JS/BaseLibrary/RegExp.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text.RegularExpressions;
 using NiL.JS.Core;
 using NiL.JS.Core.Interop;
@@ -10,16 +10,33 @@ namespace NiL.JS.BaseLibrary
 #endif
     public sealed class RegExp : CustomType
     {
+        private struct RegExpCacheItem
+        {
+            public string key;
+            public Regex re;
+            public RegExpCacheItem(string key, Regex re)
+            {
+                this.key = key;
+                this.re = re;
+            }
+        }
+        private static RegExpCacheItem[] _cache = new RegExpCacheItem[_cacheSize];
+        private static int _cacheIndex = -1;
+        private const int _cacheSize = 16;
+
+
         private string _source;
-        private JSValue lIndex;
-        internal Regex _Regex;
+        private JSValue _lastIndex;
+        internal Regex _regex;
 
         [DoNotEnumerate]
         public RegExp()
         {
             _source = "";
             _global = false;
-            _Regex = new Regex("");
+            _sticky = false;
+            _unicode = false;
+            _regex = new Regex("");
         }
 
         private void makeRegex(Arguments args)
@@ -30,38 +47,34 @@ namespace NiL.JS.BaseLibrary
                 if (args.GetProperty("length")._iValue > 1 && args[1]._valueType > JSValueType.Undefined)
                     ExceptionHelper.Throw(new TypeError("Cannot supply flags when constructing one RegExp from another"));
                 _oValue = ptrn._oValue;
-                _Regex = (ptrn.Value as RegExp)._Regex;
-                _global = (ptrn.Value as RegExp).global;
+                _regex = (ptrn.Value as RegExp)._regex;
+                _global = (ptrn.Value as RegExp)._global;
+                _sticky = (ptrn.Value as RegExp)._sticky;
+                _unicode = (ptrn.Value as RegExp)._unicode;
                 _source = (ptrn.Value as RegExp)._source;
                 return;
             }
             var pattern = ptrn._valueType > JSValueType.Undefined ? ptrn.ToString() : "";
             var flags = args.GetProperty("length")._iValue > 1 && args[1]._valueType > JSValueType.Undefined ? args[1].ToString() : "";
-            makeRegex(pattern, flags);
+            makeRegex(pattern, flags, false);
         }
 
-        private void makeRegex(string pattern, string flags)
+        private void makeRegex(string pattern, string flags, bool unescapeFlags)
         {
             pattern = pattern ?? "null";
-            flags = flags ?? "~";
+            flags = flags ?? "null";
             _global = false;
+            _sticky = false;
+            _unicode = false;
             try
             {
                 var options = RegexOptions.ECMAScript | RegexOptions.CultureInvariant;
+
+                if (unescapeFlags)
+                    flags = Tools.Unescape(flags, false, true, false, true);
                 for (int i = 0; i < flags.Length; i++)
                 {
-                    char c = flags[i];
-                    if (c == '\\')
-                    {
-                        int len = 1;
-                        if (flags[i + 1] == 'u')
-                            len = 5;
-                        else if (flags[i + 1] == 'x')
-                            len = 3;
-                        c = Tools.Unescape(flags.Substring(i, len + 1), false)[0];
-                        i += len;
-                    }
-                    switch (c)
+                    switch (flags[i])
                     {
                         case 'i':
                             {
@@ -84,6 +97,20 @@ namespace NiL.JS.BaseLibrary
                                 _global = true;
                                 break;
                             }
+                        case 'u':
+                            {
+                                if (_unicode)
+                                    ExceptionHelper.Throw((new SyntaxError("Try to double use RegExp flag \"" + flags[i] + '"')));
+                                _unicode = true;
+                                break;
+                            }
+                        case 'y':
+                            {
+                                if (_sticky)
+                                    ExceptionHelper.Throw((new SyntaxError("Try to double use RegExp flag \"" + flags[i] + '"')));
+                                _sticky = true;
+                                break;
+                            }
                         default:
                             {
                                 ExceptionHelper.Throw((new SyntaxError("Invalid RegExp flag \"" + flags[i] + '"')));
@@ -92,7 +119,31 @@ namespace NiL.JS.BaseLibrary
                     }
                 }
                 _source = pattern;
-                _Regex = new Regex(Tools.Unescape(pattern, false, false, true), options);
+
+                string label = _source + "/"
+                + ((options & System.Text.RegularExpressions.RegexOptions.IgnoreCase) != 0 ? "i" : "")
+                + ((options & System.Text.RegularExpressions.RegexOptions.Multiline) != 0 ? "m" : "")
+                + (_unicode ? "u" : "");
+                if (_cacheIndex >= 0)
+                {
+                    int _cacheSizeMinusOne = _cacheSize - 1;
+                    for (var i = _cacheSize + _cacheIndex; i > _cacheIndex; i--)
+                    {
+                        if (_cache[i & _cacheSizeMinusOne].key == label)
+                        {
+                            _regex = _cache[i & _cacheSizeMinusOne].re;
+                            return;
+                        }
+                    }
+                }
+
+                // We will need a transpiler to support ES6 Unicode regular expressions.
+                // For now, I settled with Unicode escapes like \u{41} being supported.
+                _regex = new Regex(Tools.Unescape(pattern, false, false, true, _unicode), options);
+
+                _cacheIndex = (_cacheIndex + 1) % _cacheSize;
+                _cache[_cacheIndex].key = label;
+                _cache[_cacheIndex].re = _regex;
             }
             catch (ArgumentException e)
             {
@@ -108,34 +159,15 @@ namespace NiL.JS.BaseLibrary
 
         [DoNotEnumerate]
         public RegExp(string pattern, string flags)
+            : this(pattern, flags, false)
         {
-            makeRegex(pattern, flags);
         }
 
-        [Field]
-        [ReadOnly]
-        [DoNotDelete]
         [DoNotEnumerate]
-        [NotConfigurable]
-        public Boolean ignoreCase
+        [Hidden]
+        public RegExp(string pattern, string flags, bool unescapeFlags)
         {
-            get
-            {
-                return (_Regex.Options & System.Text.RegularExpressions.RegexOptions.IgnoreCase) != 0;
-            }
-        }
-
-        [Field]
-        [ReadOnly]
-        [DoNotDelete]
-        [DoNotEnumerate]
-        [NotConfigurable]
-        public Boolean multiline
-        {
-            get
-            {
-                return (_Regex.Options & System.Text.RegularExpressions.RegexOptions.Multiline) != 0;
-            }
+            makeRegex(pattern, flags, unescapeFlags);
         }
 
         internal bool _global;
@@ -150,6 +182,62 @@ namespace NiL.JS.BaseLibrary
             get
             {
                 return _global;
+            }
+        }
+
+        [Field]
+        [ReadOnly]
+        [DoNotDelete]
+        [DoNotEnumerate]
+        [NotConfigurable]
+        public Boolean ignoreCase
+        {
+            get
+            {
+                return (_regex.Options & System.Text.RegularExpressions.RegexOptions.IgnoreCase) != 0;
+            }
+        }
+
+        [Field]
+        [ReadOnly]
+        [DoNotDelete]
+        [DoNotEnumerate]
+        [NotConfigurable]
+        public Boolean multiline
+        {
+            get
+            {
+                return (_regex.Options & System.Text.RegularExpressions.RegexOptions.Multiline) != 0;
+            }
+        }
+
+        internal bool _sticky;
+        [Field]
+        [ReadOnly]
+        [DoNotDelete]
+        [DoNotEnumerate]
+        [NotConfigurable]
+        public Boolean sticky
+        {
+            [Hidden]
+            get
+            {
+                return _sticky;
+            }
+        }
+
+        internal bool _unicode;
+        [Field]
+        [ReadOnly]
+        [DoNotDelete]
+        [DoNotEnumerate]
+        [NotConfigurable]
+        public Boolean unicode
+        {
+            [Hidden]
+            get
+            {
+                return _unicode;
             }
         }
 
@@ -174,11 +262,11 @@ namespace NiL.JS.BaseLibrary
         {
             get
             {
-                return lIndex ?? (lIndex = 0);
+                return _lastIndex ?? (_lastIndex = 0);
             }
             set
             {
-                lIndex = (value ?? JSValue.undefined).CloneImpl(false);
+                _lastIndex = (value ?? JSValue.undefined).CloneImpl(false);
             }
         }
 
@@ -193,61 +281,91 @@ namespace NiL.JS.BaseLibrary
         public JSValue exec(JSValue arg)
         {
             string input = (arg ?? "null").ToString();
-            lIndex = Tools.JSObjectToNumber(lastIndex);
-            if ((lIndex._attributes & JSValueAttributesInternal.SystemObject) != 0)
-                lIndex = lIndex.CloneImpl(false);
-            if (lIndex._valueType == JSValueType.Double)
+
+            if (!_global && !_sticky)
             {
-                lIndex._valueType = JSValueType.Integer;
-                lIndex._iValue = (int)lIndex._dValue;
+                // non-global and/or non-sticky matching doesn't use lastIndex
+
+                var m = _regex.Match(input);
+                if (!m.Success)
+                    return JSValue.@null;
+
+                var res = new Array(m.Groups.Count);
+                for (int i = 0; i < m.Groups.Count; i++)
+                    res._data[i] = m.Groups[i].Success ? (JSValue)m.Groups[i].Value : null;
+
+                res.DefineProperty("index").Assign(m.Index);
+                res.DefineProperty("input").Assign(input);
+
+                return res;
             }
-            if (lIndex._iValue < 0)
-                lIndex._iValue = 0;
-            if (lIndex._iValue >= input.Length && input.Length > 0)
+            else
             {
-                lIndex._iValue = 0;
-                return JSValue.@null;
+                _lastIndex = Tools.JSObjectToNumber(_lastIndex);
+                if ((_lastIndex._attributes & JSValueAttributesInternal.SystemObject) != 0)
+                    _lastIndex = _lastIndex.CloneImpl(false);
+                if (_lastIndex._valueType == JSValueType.Double)
+                {
+                    _lastIndex._valueType = JSValueType.Integer;
+                    _lastIndex._iValue = (int)_lastIndex._dValue;
+                }
+
+                int li = (_lastIndex._iValue < 0) ? 0 : _lastIndex._iValue;
+                _lastIndex._iValue = 0;
+
+                if (li >= input.Length && input.Length > 0)
+                    return JSValue.@null;
+
+                var m = _regex.Match(input, li);
+                if (!m.Success || (_sticky && m.Index != li))
+                    return JSValue.@null;
+
+                var res = new Array(m.Groups.Count);
+                for (int i = 0; i < m.Groups.Count; i++)
+                    res._data[i] = m.Groups[i].Success ? (JSValue)m.Groups[i].Value : null;
+
+                _lastIndex._iValue = m.Index + m.Length;
+
+                res.DefineProperty("index").Assign(m.Index);
+                res.DefineProperty("input").Assign(input);
+
+                return res;
             }
-            var m = _Regex.Match(input, lIndex._iValue);
-            if (!m.Success)
-            {
-                lIndex._iValue = 0;
-                return JSValue.@null;
-            }
-            var res = new Array(m.Groups.Count);
-            for (int i = 0; i < m.Groups.Count; i++)
-                res._data[i] = m.Groups[i].Success ? (JSValue)m.Groups[i].Value : null;
-            if (_global)
-                lIndex._iValue = m.Index + m.Length;
-            res.DefineProperty("index").Assign(m.Index);
-            res.DefineProperty("input").Assign(input);
-            return res;
         }
 
         [DoNotEnumerate]
         public JSValue test(JSValue arg)
         {
+            // definition: exec(arg) != null
+
             string input = (arg ?? "null").ToString();
-            lIndex = Tools.JSObjectToNumber(lIndex);
-            if (lIndex._valueType == JSValueType.Double)
+
+            if (!_global && !_sticky)
+                return _regex.IsMatch(input);
+
+
+            _lastIndex = Tools.JSObjectToNumber(_lastIndex);
+            if ((_lastIndex._attributes & JSValueAttributesInternal.SystemObject) != 0)
+                _lastIndex = _lastIndex.CloneImpl(false);
+            if (_lastIndex._valueType == JSValueType.Double)
             {
-                lIndex._valueType = JSValueType.Integer;
-                lIndex._iValue = (int)lIndex._dValue;
+                _lastIndex._valueType = JSValueType.Integer;
+                _lastIndex._iValue = (int)_lastIndex._dValue;
             }
-            if (lIndex._iValue >= input.Length || lIndex._iValue < 0)
-            {
-                lIndex._iValue = 0;
+
+            int li = (_lastIndex._iValue < 0) ? 0 : _lastIndex._iValue;
+            _lastIndex._iValue = 0;
+
+            if (li >= input.Length && input.Length > 0)
                 return false;
-            }
-            var m = _Regex.Match(input, lIndex._iValue);
-            if (!m.Success)
-            {
-                lIndex._iValue = 0;
+
+            var m = _regex.Match(input, li);
+            if (!m.Success || (_sticky && m.Index != li))
                 return false;
-            }
-            if (_global)
-                lastIndex._iValue = m.Index + m.Length;
-            return m.Success;
+
+            _lastIndex._iValue = m.Index + m.Length;
+
+            return true;
         }
 
 #if !WRC
@@ -263,9 +381,11 @@ namespace NiL.JS.BaseLibrary
         public override string ToString()
         {
             return "/" + _source + "/"
-                + ((_Regex.Options & System.Text.RegularExpressions.RegexOptions.IgnoreCase) != 0 ? "i" : "")
-                + ((_Regex.Options & System.Text.RegularExpressions.RegexOptions.Multiline) != 0 ? "m" : "")
-                + (_global ? "g" : "");
+                + (_global ? "g" : "")
+                + ((_regex.Options & System.Text.RegularExpressions.RegexOptions.IgnoreCase) != 0 ? "i" : "")
+                + ((_regex.Options & System.Text.RegularExpressions.RegexOptions.Multiline) != 0 ? "m" : "")
+                + (_unicode ? "u" : "")
+                + (_sticky ? "y" : "");
         }
     }
 }
