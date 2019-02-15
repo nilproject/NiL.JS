75a3103e6985486c336496ef1632cf0140f8fbdb
Дмитрий <polhovich.dm@mail.ru>
2019-02-15 13:39:42 +0300

    Add extension method "GetDefinedOr<T>(default value)"

--- NiL.JS/Extensions/JSValueExtensions.cs
+++ NiL.JS/Extensions/JSValueExtensions.cs
@@ -27,71 +27,71 @@ namespace NiL.JS.Extensions
 #endif
             {
                 case TypeCode.Boolean:
-                    {
-                        return self.Is(JSValueType.Boolean);
-                    }
+                {
+                    return self.Is(JSValueType.Boolean);
+                }
                 case TypeCode.Byte:
-                    {
-                        return self.Is(JSValueType.Integer) && (self._iValue & ~byte.MaxValue) == 0;
-                    }
+                {
+                    return self.Is(JSValueType.Integer) && (self._iValue & ~byte.MaxValue) == 0;
+                }
                 case TypeCode.Char:
-                    {
-                        return (self != null
-                            && self._valueType == JSValueType.Object
-                            && self._oValue is char);
-                    }
+                {
+                    return (self != null
+                        && self._valueType == JSValueType.Object
+                        && self._oValue is char);
+                }
                 case TypeCode.Decimal:
-                    {
-                        return false;
-                    }
+                {
+                    return false;
+                }
                 case TypeCode.Double:
-                    {
-                        return self.Is(JSValueType.Double);
-                    }
+                {
+                    return self.Is(JSValueType.Double);
+                }
                 case TypeCode.Int16:
-                    {
-                        return self.Is(JSValueType.Integer) && (self._iValue & ~ushort.MaxValue) == 0;
-                    }
+                {
+                    return self.Is(JSValueType.Integer) && (self._iValue & ~ushort.MaxValue) == 0;
+                }
                 case TypeCode.Int32:
-                    {
-                        return self.Is(JSValueType.Integer);
-                    }
+                {
+                    return self.Is(JSValueType.Integer);
+                }
                 case TypeCode.Int64:
-                    {
-                        return self.Is(JSValueType.Integer) || (self.Is(JSValueType.Double) && self._dValue == (long)self._dValue);
-                    }
+                {
+                    return self.Is(JSValueType.Integer) || (self.Is(JSValueType.Double) && self._dValue == (long)self._dValue);
+                }
                 case TypeCode.Object:
-                    {
-                        return self.Value is T;
-                    }
+                {
+                    return self.Value is T;
+                }
                 case TypeCode.SByte:
-                    {
-                        return self.Is(JSValueType.Integer) && (self._iValue & ~byte.MaxValue) == 0;
-                    }
+                {
+                    return self.Is(JSValueType.Integer) && (self._iValue & ~byte.MaxValue) == 0;
+                }
                 case TypeCode.Single:
-                    {
-                        return self.Is(JSValueType.Double) && (float)self._dValue == self._dValue;
-                    }
+                {
+                    return self.Is(JSValueType.Double) && (float)self._dValue == self._dValue;
+                }
                 case TypeCode.String:
-                    {
-                        return self.Is(JSValueType.String);
-                    }
+                {
+                    return self.Is(JSValueType.String);
+                }
                 case TypeCode.UInt16:
-                    {
-                        return self.Is(JSValueType.Integer) && (self._iValue & ~ushort.MaxValue) == 0;
-                    }
+                {
+                    return self.Is(JSValueType.Integer) && (self._iValue & ~ushort.MaxValue) == 0;
+                }
                 case TypeCode.UInt32:
-                    {
-                        return self.Is(JSValueType.Integer);
-                    }
+                {
+                    return self.Is(JSValueType.Integer);
+                }
                 case TypeCode.UInt64:
-                    {
-                        return (self.Is(JSValueType.Integer) && self._iValue >= 0) || (self.Is(JSValueType.Double) && self._dValue == (ulong)self._dValue);
-                    }
+                {
+                    return (self.Is(JSValueType.Integer) && self._iValue >= 0) || (self.Is(JSValueType.Double) && self._dValue == (ulong)self._dValue);
+                }
                 default:
-                    {
-                        return false;
-                    }
+                {
+                    return false;
+                }
             }
         }
 
@@ -103,80 +103,117 @@ namespace NiL.JS.Extensions
             switch (Type.GetTypeCode(typeof(T)))
 #endif
             {
-                case TypeCode.Boolean:
-                    return (T)(object)(bool)self; // оптимизатор разруливает такой каскад преобразований
-                case TypeCode.Byte:
-                    {
-                        return (T)(object)(byte)Tools.JSObjectToInt32(self);
-                    }
-                case TypeCode.Char:
-                    {
-                        if (self._valueType == JSValueType.Object
-                            && self._oValue is char)
-                            return (T)self._oValue;
-                        break;
-                    }
-                case TypeCode.Decimal:
-                    {
-                        return (T)(object)(decimal)Tools.JSObjectToDouble(self);
-                    }
                 case TypeCode.Double:
-                    {
-                        return (T)(object)Tools.JSObjectToDouble(self);
-                    }
+                    return GetDefinedOr<T>(self, (T)(object)double.NaN);
+            }
+
+            return GetDefinedOr<T>(self, default(T));
+        }
+
+        public static T GetDefinedOr<T>(this JSValue self, T defaultValue)
+        {
+            if (!self.Defined)
+                return defaultValue;
+
+#if PORTABLE || NETCORE
+            switch (typeof(T).GetTypeCode())
+#else
+            switch (Type.GetTypeCode(typeof(T)))
+#endif
+            {
+                case TypeCode.Boolean:
+                {
+                    return (T)(object)(bool)self; // оптимизатор разруливает такой каскад преобразований
+                }
+
+                case TypeCode.Byte:
+                {
+                    return (T)(object)(byte)Tools.JSObjectToInt32(self);
+                }
+
+                case TypeCode.Char:
+                {
+                    if (self._valueType == JSValueType.Object
+                        && self._oValue is char)
+                        return (T)self._oValue;
+                    break;
+                }
+
+                case TypeCode.Decimal:
+                {
+                    var value = Tools.JSObjectToDouble(self);
+                    if (double.IsNaN(value))
+                        value = 0;
+
+                    return (T)(object)(decimal)value;
+                }
+
+                case TypeCode.Double:
+                {
+                    return (T)(object)Tools.JSObjectToDouble(self);
+                }
+
                 case TypeCode.Int16:
-                    {
-                        return (T)(object)(Int16)Tools.JSObjectToInt32(self);
-                    }
+                {
+                    return (T)(object)(Int16)Tools.JSObjectToInt32(self);
+                }
+
                 case TypeCode.Int32:
-                    {
-                        return (T)(object)Tools.JSObjectToInt32(self);
-                    }
+                {
+                    return (T)(object)Tools.JSObjectToInt32(self);
+                }
+
                 case TypeCode.Int64:
-                    {
-                        return (T)(object)Tools.JSObjectToInt64(self);
-                    }
+                {
+                    return (T)(object)Tools.JSObjectToInt64(self);
+                }
+
                 case TypeCode.Object:
+                {
+                    if (self.Value is Function && typeof(Delegate).IsAssignableFrom(typeof(T)))
+                        return ((Function)self.Value).MakeDelegate<T>();
+
+                    if (typeof(T).IsAssignableFrom(self.GetType()))
+                        return (T)(object)self;
+
+                    try
                     {
-                        if (self.Value is Function && typeof(Delegate).IsAssignableFrom(typeof(T)))
-                            return ((Function)self.Value).MakeDelegate<T>();
-
-                        if (typeof(T).IsAssignableFrom(self.GetType()))
-                            return (T)(object)self;
-
-                        try
-                        {
-                            return (T)(Tools.convertJStoObj(self, typeof(T), true) ?? self.Value);
-                        }
-                        catch (InvalidCastException)
-                        {
-                            return default(T);
-                        }
+                        return (T)(Tools.convertJStoObj(self, typeof(T), true) ?? self.Value);
                     }
+                    catch (InvalidCastException)
+                    {
+                        return default(T);
+                    }
+                }
+
                 case TypeCode.SByte:
-                    {
-                        return (T)(object)(sbyte)Tools.JSObjectToInt32(self);
-                    }
+                {
+                    return (T)(object)(sbyte)Tools.JSObjectToInt32(self);
+                }
                 case TypeCode.Single:
-                    {
-                        return (T)(object)(float)Tools.JSObjectToDouble(self);
-                    }
+                {
+                    return (T)(object)(float)Tools.JSObjectToDouble(self);
+                }
+
                 case TypeCode.String:
-                    {
-                        return (T)(object)self.ToString();
-                    }
+                {
+                    return (T)(object)self.ToString();
+                }
+
                 case TypeCode.UInt16:
-                    {
-                        return (T)(object)(ushort)Tools.JSObjectToInt32(self);
-                    }
+                {
+                    return (T)(object)(ushort)Tools.JSObjectToInt32(self);
+                }
+
                 case TypeCode.UInt32:
-                    {
-                        return (T)(object)(uint)Tools.JSObjectToInt32(self);
-                    }
+                {
+                    return (T)(object)(uint)Tools.JSObjectToInt32(self);
+                }
+
                 case TypeCode.UInt64:
-                    {
-                        return (T)(object)(ulong)Tools.JSObjectToInt64(self);
-                    }
+                {
+                    return (T)(object)(ulong)Tools.JSObjectToInt64(self);
+                }
             }
 
             throw new InvalidCastException();
