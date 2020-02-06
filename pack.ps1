bd29352416cc6039201e68886d74fadf857ef6b5
Дмитрий <polhovich.dm@mail.ru>
2020-02-06 19:53:48 +0300

    Update pack.ps

--- pack.ps1
+++ pack.ps1
@@ -12,19 +12,9 @@ mkdir nuget -erroraction 'silentlycontinue'
 {
     internal const string Version = ""2.5.$(git rev-list --count develop)"";
 }")
-& "\Program Files (x86)\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\msbuild" nil.js\nil.js.csproj -p:VisualStudioVersion=15.0
-& "\Program Files (x86)\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\msbuild" nil.js\nil.js.csproj -p:VisualStudioVersion=15.0 -p:OutputPath=..\net461\
-& "\Program Files (x86)\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\msbuild" nil.js\nil.js.csproj -p:VisualStudioVersion=11.0 -p:OutputPath=..\net45\
-& "\Program Files (x86)\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\msbuild" nil.js\nil.js.csproj -p:VisualStudioVersion=10.0 -p:OutputPath=..\net40\
-& "\Program Files (x86)\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\msbuild" NiL.JS.Portable\nil.js.portable.csproj /p:Configuration=Release
-cd nil.js
-./nuget pack -OutputDirectory ../nuget
-cd ..
-rd net40 -Recurse -Force
-cd NiL.JS.NetCore
+cd NiL.JS
 dotnet build -c Release -property:VersionPrefix=2.5.$(git rev-list --count develop)
-dotnet pack -c Release
+dotnet pack -c Release -property:VersionPrefix=2.5.$(git rev-list --count develop)
 cd ..
 # git stash pop
-rd net461 -Recurse -Force
-rd net45 -Recurse -Force
+pause
