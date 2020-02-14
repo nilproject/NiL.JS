edc941776c8799563467c78663a745c578f0754a
Дмитрий <polhovich.dm@mail.ru>
2020-02-14 21:19:54 +0300

    Disable signing for CI

--- NiL.JS/makeSymLinks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ln -rs ../NiL.JS/BaseLibrary . 2> /dev/null
-ln -rs ../NiL.JS/Core . 2> /dev/null
-ln -rs ../NiL.JS/Expressions . 2> /dev/null
-ln -rs ../NiL.JS/Extensions . 2> /dev/null
-ln -rs ../NiL.JS/Properties . 2> /dev/null
-ln -rs ../NiL.JS/Statements . 2> /dev/null
