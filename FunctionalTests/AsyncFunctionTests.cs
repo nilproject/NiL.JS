bc98f628cc5270d2378bb86bb23e59b457a838a5
Josh Johnson <josh.johnson@truecommerce.com>
2020-07-22 13:45:15 -0400

    Add Passing and Failing Async Function Tests

---
+++ FunctionalTests/AsyncFunctionTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using NiL.JS.BaseLibrary;
+using NiL.JS.Core;
+using NiL.JS.Extensions;
+
+namespace FunctionalTests
+{
+    [TestClass]
+    public class AsyncFunctionTests
+    {
+        [TestMethod]
+        public async Task MarshalResolvedPromiseAsTask()
+        {
+            var context = new Context();
+
+            var result = await context.Eval("function getPromise() { return new Promise((a, b) => { a('test'); }); } async function test() { return await getPromise(); } test();").As<Promise>().Task;
+
+            Assert.AreEqual("test", result.ToString());
+        }
+
+        [TestMethod]
+        public async Task MarshalRejectedPromiseAsFailedTask()
+        {
+            var context = new Context();
+
+            await Assert.ThrowsExceptionAsync<Exception>(async () =>
+            {
+                await context.Eval("function getPromise() { return new Promise((a, b) => { b('test'); }); } async function test() { return await getPromise(); } test();").As<Promise>().Task;
+            });
+        }
+    }
+}
