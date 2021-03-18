a73175a5688fa55a79f698b47c00ca034e8ab840
Дмитрий <polhovich.dm@mail.ru>
2021-03-18 17:55:54 +0300

    Enable unconditionaly SignAssembly in pack script

--- pack.ps1
+++ pack.ps1
@@ -10,8 +10,8 @@ mkdir nuget -erroraction 'silentlycontinue'
     internal const string Year = ""$(get-date -Format yyyy)"";
 }")
 cd NiL.JS
-dotnet build -c Release -property:VersionPrefix=2.5.$(git rev-list --count develop)
-dotnet pack -c Release -property:VersionPrefix=2.5.$(git rev-list --count develop)
+dotnet build -c Release -property:VersionPrefix=2.5.$(git rev-list --count develop) -property:SignAssembly=true
+dotnet pack -c Release -property:VersionPrefix=2.5.$(git rev-list --count develop) -property:SignAssembly=true
 copy bin/release/NiL.JS.2.5.$(git rev-list --count develop).nupkg ../nuget/NiL.JS.2.5.$(git rev-list --count develop).nupkg
 cd ..
 # git stash pop
