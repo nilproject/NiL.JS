5e6d7e8a26642749151a547ea1e6970d42cd9b9f
Дмитрий <polhovich.dm@mail.ru>
2021-11-08 17:35:52 +0300

    Fix pack.ps1

--- pack.ps1
+++ pack.ps1
@@ -1,18 +1,16 @@
-# git stash
 echo $(
 rd nil.js\bin -Force -Recurse -erroraction 'silentlycontinue'
 rd nil.js\obj -Force -Recurse -erroraction 'silentlycontinue'
 mkdir nuget -erroraction 'silentlycontinue'
 ) > $null
+$REVISION=$(git rev-list --count origin/develop)
 [System.IO.File]::WriteAllText("$(get-location)\\NiL.JS\\Properties\\InternalInfo.cs","internal static class InternalInfo
 {
-    internal const string Version = ""2.5.$(git rev-list --count develop)"";
+    internal const string Version = ""2.5.$($REVISION)"";
     internal const string Year = ""$(get-date -Format yyyy)"";
 }")
 cd NiL.JS
-dotnet build -c Release -property:VersionPrefix=2.5.$(git rev-list --count develop) -property:SignAssembly=true
-dotnet pack -c Release -property:VersionPrefix=2.5.$(git rev-list --count develop) -property:SignAssembly=true
-copy bin/release/NiL.JS.2.5.$(git rev-list --count develop).nupkg ../nuget/NiL.JS.2.5.$(git rev-list --count develop).nupkg
+dotnet build -c Release -property:VersionPrefix=2.5.$($REVISION) -property:SignAssembly=true
+dotnet pack -c Release -property:VersionPrefix=2.5.$($REVISION) -property:SignAssembly=true
+copy bin/release/NiL.JS.2.5.$($REVISION).nupkg ../nuget/NiL.JS.2.5.$($REVISION).nupkg
 cd ..
-# git stash pop
-pause
