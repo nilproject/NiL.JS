9b4a5b51ae3f5badb9a28622657eb9b8771bbfda
Michael Kriese <michael.kriese@visualon.de>
2021-11-29 16:57:33 +0100

    test: add more tests

--- Tests/BaseLibrary/SetTests.cs
+++ Tests/BaseLibrary/SetTests.cs
@@ -19,7 +19,7 @@ namespace Tests.BaseLibrary
         }
 
         [TestMethod]
-        public void RemovesElements()
+        public void ContainsElement()
         {
             var script = @"
 const s=new Set([1,2,3,4]);
@@ -30,5 +30,33 @@ s.has(2);";
             Assert.AreEqual(JSValueType.Boolean, result.ValueType);
             Assert.AreEqual(true, result.Value);
         }
+
+
+        [TestMethod]
+        public void DeletesElement()
+        {
+            var script = @"
+const s=new Set([1,2,3,4]);
+s.delete(2);";
+            var context = new Context();
+            var result = context.Eval(script);
+
+            Assert.AreEqual(JSValueType.Boolean, result.ValueType);
+            Assert.AreEqual(true, result.Value);
+        }
+
+        [TestMethod]
+        public void DontAddElement()
+        {
+            var script = @"
+const s = new Set([1,2,3,4]);
+s.add(2).size;";
+            var context = new Context();
+            var result = context.Eval(script);
+
+            Assert.AreEqual(JSValueType.Integer, result.ValueType);
+            Assert.AreEqual(4, result.Value);
+        }
+
     }
 }
