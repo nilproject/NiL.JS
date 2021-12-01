9ded843dce9f3346de5a0848d5261fc6b97d2a91
Дмитрий <polhovich.dm@mail.ru>
2021-12-01 16:59:52 +0300

    Tests for Optional chaining

---
+++ Tests/Fuzz/OptionalChaining.js
@@ -0,0 +1,21 @@
+﻿console.assert(null?.a == undefined);
+
+console.assert(undefined?.a == undefined);
+
+console.assert(null?.["a"] == undefined);
+
+console.assert(null?.(a) == undefined);
+
+var variable = 1;
+console.assert(null?.(variable = 2) == undefined);
+console.assert(undefined?.(variable = 2) == undefined);
+console.assert(variable == 1);
+
+console.assert(null?.[variable = 2] == undefined);
+console.assert(undefined?.[variable = 2] == undefined);
+console.assert(variable == 1);
+
+var object = { foo: { boo: () => "hello" } };
+console.assert(object?.foo?.boo() == "hello");
+
+console.assert(false?.5:true);
