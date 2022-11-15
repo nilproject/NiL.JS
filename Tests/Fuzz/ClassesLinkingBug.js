f591f6835e645b026d42b6c914ba6bd28535ba84
Дмитрий <polhovich.dm@mail.ru>
2022-11-15 13:29:35 +0300

    Add some tests

---
+++ Tests/Fuzz/ClassesLinkingBug.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    class NestedClass {
+        get someValue() {
+            return 1;
+        }
+    }
+
+    var { someValue } = new NestedClass;
+
+    if (someValue != 1)
+        console.error("Fail");
+})
---
+++ Tests/Fuzz/JsonFuzz.js
@@ -0,0 +1,11 @@
+﻿var json = JSON.stringify({ o: { v: 1, s: 'str' }, a: 'a', b: 1 }, undefined, '  ');
+var expected = `{
+  "o": {
+    "v": 1,
+    "s": "str"
+  },
+  "a": "a",
+  "b": 1
+}`;
+if (json != expected)
+    console.log(json);
