cc0154bb2423be91b55839e53119db2a48100521
Дмитрий <polhovich.dm@mail.ru>
2023-01-18 20:56:00 +0300

    Update ci script

--- pack.ps1
+++ pack.ps1
@@ -6,11 +6,11 @@ mkdir nuget -erroraction 'silentlycontinue'
 $REVISION=$(git rev-list --count origin/develop)
 [System.IO.File]::WriteAllText("$(get-location)\\NiL.JS\\Properties\\InternalInfo.cs","internal static class InternalInfo
 {
-    internal const string Version = ""2.5.$($REVISION)"";
+    internal const string Version = ""$VERSION.$($REVISION)"";
     internal const string Year = ""$(get-date -Format yyyy)"";
 }")
 cd NiL.JS
-dotnet build -c Release -property:VersionPrefix=2.5.$($REVISION) -property:SignAssembly=true
-dotnet pack -c Release -property:VersionPrefix=2.5.$($REVISION) -property:SignAssembly=true
-mv -Force bin/release/NiL.JS.2.5.$($REVISION).nupkg ../nuget/NiL.JS.2.5.$($REVISION).nupkg
+dotnet build -c Release -property:VersionPrefix=$VERSION.$($REVISION) -property:SignAssembly=true
+dotnet pack -c Release -property:VersionPrefix=$VERSION.$($REVISION) -property:SignAssembly=true
+mv -Force bin/release/NiL.JS.$VERSION.$($REVISION).nupkg ../nuget/NiL.JS.$VERSION.$($REVISION).nupkg
 cd ..
