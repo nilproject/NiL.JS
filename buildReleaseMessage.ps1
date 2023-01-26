ea6937db26fec80265132b57cc1f719ed4d21242
Дмитрий <polhovich.dm@mail.ru>
2023-01-26 17:45:41 +0300

    Fix release notes script

--- buildReleaseMessage.ps1
+++ buildReleaseMessage.ps1
@@ -3,7 +3,9 @@ $URL=$(git remote get-url origin).Replace('.git', '') + "/commit/";
 $COMMITS=$(git cherry $LAST_TAG HEAD)
 foreach ($commit in $COMMITS){ 
     $commit=$commit.Replace('+', '').Trim();
-    $message=(iex (echo "git show -s --format=%B $($commit)")); 
-    $message="[$(iex "echo $($message.Replace("'", "''''"))")](" + $URL + $commit + ")";
-    echo $message;
+    $message=$(iex $(echo "git show -s --format=%B $($commit)"))[0];
+    $message=$message.Replace("'", "''''");
+    $message="echo ""$message""";
+    $message="[$(iex "$message")]($URL" + "$commit)";
+    echo $message;    
 }
